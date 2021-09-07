6eca81a6a52312a896406a3b368e6607de29cece
Qiliang Yang <qiliang.yang@klook.com>
2021-09-07 18:35:35 +0800

    feat: 多线程初步调通

--- remove-ts-node/auto-sh.js
+++ remove-ts-node/auto-sh.js
@@ -1,4 +1,4 @@
-const { exec } = require('child_process');
+
 
 // 输出当前目录（不一定是代码所在的目录）下的文件和文件夹
 console.log('-- 1: ', 1)
---
+++ remove-ts-node/t.js
@@ -0,0 +1,2 @@
+const fs = require('fs')
+fs.writeFileSync('./1.json', 1, 'utf-8')
